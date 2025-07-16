fc4879c45dcb460cec34a2352d92f2537b920764
Rafael <rafangarac@gmail.com>
2025-07-15 21:25:07 -0500

    Initial project setup with Spring Boot and selected dependencies

---
+++ src/main/java/com/fondosGFT/fondosGFT/FondosGftApplication.java
@@ -0,0 +1,13 @@
+package com.fondosGFT.fondosGFT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FondosGftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FondosGftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fondosGFT/fondosGFT/FondosGftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fondosGFT.fondosGFT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FondosGftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
